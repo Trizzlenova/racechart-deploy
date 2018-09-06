be2db88bf02042317ccf52f4fb2b1b3f57512a52
bcorman <benjamincorman@gmail.com>
2018-09-06 16:08:15 -0700

    registered models

--- racechart_app/admin.py
+++ racechart_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Driver, Team, Race, Standing, Result
 
-# Register your models here.
+admin.site.register(Driver)
+admin.site.register(Team)
+admin.site.register(Race)
+admin.site.register(Standing)
+admin.site.register(Result)
