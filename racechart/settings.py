c9421201a4034c0326712fecd23ca88916adbb19
bcorman <benjamincorman@gmail.com>
2018-09-06 16:53:52 -0700

    fixed error in installed apps. db migration working

--- racechart/settings.py
+++ racechart/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'racechart',
+    'racechart_app',
     'django_extensions',
 ]
 
