1494c118c7e5387e8eb2ce5ab361f339f33aeede
bcorman <benjamincorman@gmail.com>
2018-09-07 16:38:22 -0700

    modified url import path

--- racechart_app/urls.py
+++ racechart_app/urls.py
@@ -18,7 +18,6 @@ urlpatterns = [
     url(r'api/standings/(?P<pk>[0-9]+)/$', views.StandingDetail.as_view()),
     url(r'api/results/$', views.ResultList.as_view()),
     url(r'api/results/(?P<pk>[0-9]+)/$', views.ResultDetail.as_view()),
-    path('getall', views.get_all_races, name='get_all_races'),
     path('drivers/', views.driver_list, name='driver_list'),
     path('drivers/<int:pk>', views.driver_detail, name='driver_detail'),
     path('standings/', views.standing_list, name='standing_list'),
