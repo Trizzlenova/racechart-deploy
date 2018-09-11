e59c36407a212b1e4f4262d3288ebc0ae4bcb089
bcorman <benjamincorman@gmail.com>
2018-09-10 21:03:41 -0700

    resized graph

--- racechart/static/script/app.js
+++ racechart/static/script/app.js
@@ -7,7 +7,7 @@ getResults();
 const createGraph = (data) => {
   // set the dimensions and margins of the graph
   var margin = {top: 20, right: 20, bottom: 30, left: 50},
-      width = 900 - margin.left - margin.right,
+      width = 750 - margin.left - margin.right,
       height = 400 - margin.top - margin.bottom;
 
   // parse the date / time
