7e629aa44c8a0a9c44c8252b6c31d59f5d2563dd
bcorman <benjamincorman@gmail.com>
2018-09-10 21:40:22 -0700

    added gridlines. created separate file to play with new graphs

--- racechart/static/script/app.js
+++ racechart/static/script/app.js
@@ -6,26 +6,38 @@ getResults();
 // Form Graph
 const createGraph = (data) => {
   // set the dimensions and margins of the graph
-  var margin = {top: 20, right: 20, bottom: 30, left: 50},
+  let margin = {top: 20, right: 20, bottom: 30, left: 50},
       width = 750 - margin.left - margin.right,
       height = 400 - margin.top - margin.bottom;
 
   // parse the date / time
-  var parseTime = d3.timeParse("%m/%d/%Y");
+  let parseTime = d3.timeParse("%m/%d/%Y");
 
   // set the ranges
-  var x = d3.scaleTime().range([0, width]);
-  var y = d3.scaleLinear().range([height, 0]);
+  let x = d3.scaleTime().range([0, width]);
+  let y = d3.scaleLinear().range([height, 0]);
+
+  // gridlines in x axis function
+  function make_x_gridlines() {
+    return d3.axisBottom(x)
+      .ticks(12)
+  }
+
+  // gridlines in y axis function
+  function make_y_gridlines() {
+    return d3.axisLeft(y)
+      .ticks(33)
+  }
 
   // define the line
-  var valueline = d3.line()
+  let valueline = d3.line()
       .x(function(d) { return x(d.date); })
       .y(function(d) { return y(d.rank); });
 
   // append the svg obgect to the body of the page
   // appends a 'group' element to 'svg'
   // moves the 'group' element to the top left margin
-  var svg = d3.select("#graphed").append("svg")
+  let svg = d3.select("#graphed").append("svg")
       .attr("width", width + margin.left + margin.right)
       .attr("height", height + margin.top + margin.bottom)
     .append("g")
@@ -36,6 +48,24 @@ const createGraph = (data) => {
   // Scale the range of the data
   x.domain(d3.extent(data, function(d) { return d.date; }));
   y.domain([0, d3.max(data, function(d) { return d.rank; })]);
+
+
+  // add the X gridlines
+  svg.append("g")
+    .attr("class", "grid")
+    .attr("transform", "translate(0," + height + ")")
+    .call(make_x_gridlines()
+      .tickSize(-height)
+      .tickFormat("")
+    )
+
+  // add the Y gridlines
+  svg.append("g")
+    .attr("class", "grid")
+    .call(make_y_gridlines()
+      .tickSize(-width)
+      .tickFormat("")
+    )
 
   // Add the valueline path.
   svg.append("path")
