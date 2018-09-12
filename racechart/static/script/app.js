7ee8a00f5566a158ba68d081879245d659f49364
bcorman <benjamincorman@gmail.com>
2018-09-12 14:02:37 -0700

    modified animation event listener

--- racechart/static/script/app.js
+++ racechart/static/script/app.js
@@ -31,4 +31,5 @@ function checkSlide(e){
     }
   })
 }
-window.addEventListener('scroll', debounce(checkSlide))
+window.addEventListener('scroll', debounce(checkSlide));
+window.addEventListener('load', debounce(checkSlide));
